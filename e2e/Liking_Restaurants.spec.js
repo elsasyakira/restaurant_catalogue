7421cb4ce8539eeca96985b5c1b71383252b119b
beliarizki26 <beliaarn2604@gmail.com>
2022-12-11 17:05:48 +0700

    update: liking test :rocket:

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -10,3 +10,53 @@ Scenario('showing empty liked restaurants', ({ I }) => {
   I.seeElement('#restaurants');
   I.see('Tidak ada restoran', '.restaurant-not__found');
 });
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restoran', '.restaurant-not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__name a');
+
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+  const likedRestaurantName = await I.grabTextFrom('.restaurant__name a');
+
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
+});
+
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restoran', '.restaurant-not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__name a');
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
+
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
+
+  I.click(likedRestaurantName);
+
+  I.seeElement('#likeButton');
+  const btnLabel = await I.grabAttributeFrom('#likeButton', 'aria-label');
+  assert.strictEqual(btnLabel, 'unlike this restaurant');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.see('Tidak ada restoran', '.restaurant-not__found');
+});
