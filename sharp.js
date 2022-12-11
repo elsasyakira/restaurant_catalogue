4f04ad516823b67b409db53a778649a5f9ba68f9
beliarizki26 <beliaarn2604@gmail.com>
2022-12-11 18:27:00 +0700

    update: rename sharps to sharp :rocket:

--- sharp.js
+++ sharp.js
@@ -2,8 +2,8 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const target = path.resolve(__dirname, 'src/public/images');
-const destination = path.resolve(__dirname, 'dist/images');
+const target = path.resolve(__dirname, 'src/public/heros/images');
+const destination = path.resolve(__dirname, 'src/public/heros/images');
 
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
@@ -13,14 +13,14 @@ fs.readdirSync(target)
   .forEach((image) => {
     sharp(`${target}/${image}`)
       .resize(800)
-      .jpeg({ quality: 30 })
+      .jpeg({ quality: 50 })
       .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
         .slice(0, -1)
         .join('.')}-large.jpg`));
 
     sharp(`${target}/${image}`)
       .resize(480)
-      .jpeg({ quality: 30 })
+      .jpeg({ quality: 50 })
       .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
         .slice(0, -1)
         .join('.')}-small.jpg`));
