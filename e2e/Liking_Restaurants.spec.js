1dff5078ef169fde1de8c62d93498fcca04c0c92
beliarizki26 <beliaarn2604@gmail.com>
2022-12-11 16:01:36 +0700

    add: liking restaurant with empty test :rocket:

---
+++ e2e/Liking_Restaurants.spec.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#restaurants');
+  I.see('Tidak ada restoran', '.restaurant-not__found');
+});
